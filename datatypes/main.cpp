048a4bd07331994a7dfc79bdd6ce31557cb8b285
realadx6sh <aadhi.babayaga@gmail.com>
2026-05-14 20:37:23 +0530

    Added : datatypes

---
+++ datatypes/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int age = 22;                   // => Integer datatype
+    float pi = 3.14;                // Float datatype
+    char alphabet = 'z';            // Character datatype
+    string name = "Adarsh";         // String datatype
+    double shareProfit = 7123.8373; // Double datatype
+    bool isMale = true;             // Boolean datatype
+    bool isMen = 1;                 // 1 => true
+    bool isChild = 0;               // 0 => false
+    bool isFemale = false;
+    long long shareHoldings = 116266272; // Long long datatype
+    return 0;
+}
