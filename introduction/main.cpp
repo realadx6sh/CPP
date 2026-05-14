563c6709579d6b475263799abb61eaff875edbde
realadx6sh <aadhi.babayaga@gmail.com>
2026-05-14 20:12:19 +0530

    Added folder : introduction

---
+++ introduction/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>  // Header file, which is used for I/O functions (cout, cin)
+using namespace std; // Containing most STD functions
+
+int main() // Main function, where execution starts
+{
+    cout << "Namaste, Duniya"; // Alternative of cout -> printf("<Text>");
+    return 0;                  // return (non-zero (1,-1, etc....)) -> Unsuccessful execution
+}
