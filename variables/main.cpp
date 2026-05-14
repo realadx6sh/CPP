a164a772fb83c851e8f4c1928a01d47a64520d13
realadx6sh <aadhi.babayaga@gmail.com>
2026-05-14 20:26:38 +0530

    Addon : Extended code with documentation

--- variables/main.cpp
+++ variables/main.cpp
@@ -4,7 +4,15 @@ using namespace std;
 int main()
 {
     // Defining variable
-    int age = 22; // <datatype> <variable_name> = <variable value>;
-    cout << "Adarsh age is:" << age << endl;
+    // int age = 22; // <datatype> <variable_name> = <variable value>;
+    // cout << "Adarsh age is:" << age << endl;
+
+    // Declaring
+    // int age; // => If try to print a declared variable without initializing it we will get some random values which is called as garbage value (eg 6262552)
+
+    // Declaring and initializing
+    int age = 22; // Note : we cant declare a variable of same name
+    cout << "The age is:" << age << endl;
+
     return 0;
 }
