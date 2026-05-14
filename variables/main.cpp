c38b1194691657dd05590b5d6eba04d4e48746f2
realadx6sh <aadhi.babayaga@gmail.com>
2026-05-14 20:16:34 +0530

    Added : variables

---
+++ variables/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // Defining variable
+    int age = 22; // <datatype> <variable_name> = <variable value>;
+    cout << "Adarsh age is:" << age << endl;
+    return 0;
+}
