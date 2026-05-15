88a980916636111cc7de4aa337699bcf7bf85ac4
realadx6sh <aadhi.babayaga@gmail.com>
2026-05-15 18:03:22 +0530

    Added : control flow

---
+++ control flow/main.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // int budget;
+    // cout << "Enter your budget:" << endl;
+    // cin >> budget;
+
+    // IF
+    // Syntax : if (condition) {
+    // Code executes if the condition is true
+    // }
+
+    // if (budget > 100000)
+    // {
+    //     cout << "You can buy an iPhone";
+    // }
+
+    //  if (condition) {
+    //      Code if true
+    // } else {
+    //      Code if false
+    // }
+
+    // if (budget > 100000)
+    // {
+    //     cout << "You can buy iPhone" << endl;
+    // }
+    // else
+    // {
+    //     cout << "You can't buy iPhone" << endl;
+    // }
+
+    // if (condition1) {
+    //     Code if condition1 is true
+    // } else if (condition2) {
+    //      Code if condition1 is false, but condition2 is true
+    // } else {
+    //      Fallback code if no conditions are true
+    // }
+
+    // if (budget > 100000)
+    // {
+    //     cout << "You can buy iPhone" << endl;
+    // }
+    // else if (budget > 50000)
+    // {
+    //     cout << "You can't buy iPhone" << endl;
+    // }
+    // else
+    // {
+    //     cout << "You can't buy a good phone" << endl;
+    // }
+
+    // int number;
+    // cout << "Enter a number: " << endl;
+    // cin >> number;
+
+    // if (number < 10)
+    // {
+    //     if (number <= 5)
+    //     {
+    //         cout << "The number maybe less than or equal to 5" << endl;
+    //     }
+    //     else
+    //     {
+    //         cout << "The number has exceeded 5" << endl;
+    //     }
+    // }
+
+    return 0;
+}
