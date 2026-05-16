795f11be8855ceec776d45073dab8fdd8157582a
realadx6sh <aadhi.babayaga@gmail.com>
2026-05-16 21:12:47 +0530

    docs : Added better comments for input

--- input/main.cpp
+++ input/main.cpp
@@ -3,9 +3,42 @@ using namespace std;
 
 int main()
 {
-    int mark; // Memory is usually allocated for it, and its value is (“garbage value”)
+    /*
+        VARIABLE DECLARATION
+
+        A variable named 'mark' is declared
+        to store an integer value entered by the user.
+
+        Since no value is assigned initially,
+        it may contain a random value
+        called a "garbage value".
+    */
+    int mark;
+
+    /*
+        OUTPUT STATEMENT
+
+        Displays a message asking the user
+        to enter their mark.
+    */
     cout << "Enter your mark :" << endl;
-    cin >> mark;                           // Taking input to variable called as mark;
-    cout << "Your mark: " << mark << endl; // Printed out marks variable
+
+    /*
+        INPUT STATEMENT
+
+        cin is used to take input from the user.
+
+        The entered value is stored inside
+        the variable 'mark'.
+    */
+    cin >> mark;
+
+    /*
+        OUTPUT STATEMENT
+
+        Displays the value entered by the user.
+    */
+    cout << "Your mark: " << mark << endl;
+
     return 0;
 }
