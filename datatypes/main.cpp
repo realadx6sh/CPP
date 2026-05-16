a8e6de6c973581a3ba71ce551294375f4e93b13b
realadx6sh <aadhi.babayaga@gmail.com>
2026-05-16 21:11:34 +0530

    docs : Added better comments for datatypes

--- datatypes/main.cpp
+++ datatypes/main.cpp
@@ -3,15 +3,86 @@ using namespace std;
 
 int main()
 {
-    int age = 22;                   // => Integer datatype
-    float pi = 3.14;                // Float datatype
-    char alphabet = 'z';            // Character datatype
-    string name = "Adarsh";         // String datatype
-    double shareProfit = 7123.8373; // Double datatype
-    bool isMale = true;             // Boolean datatype
-    bool isMen = 1;                 // 1 => true
-    bool isChild = 0;               // 0 => false
+    /*
+        DATA TYPES
+
+        Data types specify the kind of data
+        a variable can store.
+
+        Different data types use different
+        amounts of memory and store different
+        kinds of values.
+    */
+
+    /*
+        INTEGER DATA TYPE
+
+        Used to store whole numbers
+        without decimal values.
+    */
+    int age = 22;
+
+    /*
+        FLOAT DATA TYPE
+
+        Used to store decimal numbers.
+        Float provides single precision.
+    */
+    float pi = 3.14;
+
+    /*
+        CHARACTER DATA TYPE
+
+        Used to store a single character.
+        Characters are written inside single quotes.
+    */
+    char alphabet = 'z';
+
+    /*
+        STRING DATA TYPE
+
+        Used to store multiple characters
+        or text values.
+
+        Strings are written inside double quotes.
+    */
+    string name = "Adarsh";
+
+    /*
+        DOUBLE DATA TYPE
+
+        Used to store decimal numbers
+        with higher precision than float.
+    */
+    double shareProfit = 7123.8373;
+
+    /*
+        BOOLEAN DATA TYPE
+
+        Used to store logical values:
+        true or false.
+    */
+    bool isMale = true;
+
+    /*
+        In boolean values:
+        1 represents true
+        0 represents false
+    */
+    bool isMen = 1;
+    bool isChild = 0;
+
+    /*
+        Boolean variable storing false.
+    */
     bool isFemale = false;
-    long long shareHoldings = 116266272; // Long long datatype
+
+    /*
+        LONG LONG DATA TYPE
+
+        Used to store very large integer values.
+    */
+    long long shareHoldings = 116266272;
+
     return 0;
 }
