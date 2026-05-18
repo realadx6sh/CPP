809cbe6c11997423314a26c33f97bee236f207ac
realadx6sh <aadhi.babayaga@gmail.com>
2026-05-18 12:34:48 +0530

    Docs : enhanced questions and solutions (input/medium)

--- exercises/input/medium/main.cpp
+++ exercises/input/medium/main.cpp
@@ -4,20 +4,32 @@ using namespace std;
 int main()
 {
 
-    // Input full name and age and print a sentence
+    // =========================================
+    // QUESTION 1 : Input Full Name and Age
+    //               and Print a Sentence
+    // =========================================
 
     // string name;
     // int age;
+
     // cout << "Enter your full name : " << endl;
     // getline(cin, name);
+
     // cout << "Enter your age : " << endl;
     // cin >> age;
-    // cout << "Your name is " << name << ", you're " << age << " years old" << endl;
+
+    // cout << "Your name is " << name
+    //      << ", you're " << age
+    //      << " years old" << endl;
+
     // return 0;
 
-    // Input 3 numbers and print sum
+    // =========================================
+    // QUESTION 2 : Input 3 Numbers and Print Sum
+    // =========================================
 
     // int number1, number2, number3, sum;
+
     // cout << "Enter number 1 :" << endl;
     // cin >> number1;
 
@@ -28,31 +40,51 @@ int main()
     // cin >> number3;
 
     // sum = number1 + number2 + number3;
+
     // cout << "The sum of the numbers is : " << sum;
+
     // return 0;
 
-    // Input radius and calculate area of a circle
+    // =========================================
+    // QUESTION 3 : Calculate Area of a Circle
+    // =========================================
+
     // int radius;
     // float area;
+
     // const float pi = 3.14;
 
     // cout << "Enter the radius of the circle : " << endl;
     // cin >> radius;
+
     // area = pi * radius * radius;
+
     // cout << "The area of the circle is : " << area;
+
     // return 0;
 
-    // Input number and print square
+    // =========================================
+    // QUESTION 4 : Input Number and Print Square
+    // =========================================
+
     // int number;
+
     // cout << "Enter a number to find its square" << endl;
     // cin >> number;
+
     // cout << "The square is : " << number * number << endl;
+
     // return 0;
 
-    // Input marks and print pass/fail
+    // =========================================
+    // QUESTION 5 : Input Marks and Print Pass/Fail
+    // =========================================
+
     // int mark;
+
     // cout << "Enter your mark" << endl;
     // cin >> mark;
+
     // if (mark < 25)
     // {
     //     cout << "You have failed!" << endl;
@@ -61,5 +93,6 @@ int main()
     // {
     //     cout << "You have passed!" << endl;
     // }
+
     // return 0;
 }
