b53e22b4eaa1abf96b0831dad01c39f27ea98218
realadx6sh <aadhi.babayaga@gmail.com>
2026-05-19 23:17:40 +0530

    Added : conditionals

---
+++ exercises/conditionals/easy/main.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // Check positive or negative number
+
+    // int number;
+    // cout << "Enter a number to check its positive or negative : ";
+    // cin >> number;
+    // if (number >= 0)
+    // {
+    //     cout << "The number is positive";
+    // }
+    // else
+    // {
+    //     cout << "The number is negative";
+    // }
+    // return 0;
+
+    // Check even or odd
+    // int number;
+    // cout << "Enter a number to check odd or even : ";
+    // cin >> number;
+    // if (number % 2 == 0)
+    // {
+    //     cout << "The number is even";
+    // }
+    // else
+    // {
+    //     cout << "The number is odd";
+    // }
+
+    // Check age ≥ 18
+
+    int age;
+    cout << "Enter your age : ";
+    cin >> age;
+}
