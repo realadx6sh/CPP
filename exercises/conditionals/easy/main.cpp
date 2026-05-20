606573ca5b9695c30c48aa3c558d202f1e5fc666
realadx6sh <aadhi.babayaga@gmail.com>
2026-05-20 19:46:42 +0530

    Added : easy(conditionals/easy/main.cpp)

--- exercises/conditionals/easy/main.cpp
+++ exercises/conditionals/easy/main.cpp
@@ -4,83 +4,124 @@ using namespace std;
 int main()
 {
 
-    // =========================================
-    // QUESTION 1 : Check Positive or Negative Number
-    // =========================================
+    // =====================================================
+    // QUESTION 1 : Check Whether a Number is Positive or Negative
+    // =====================================================
 
+    // Variable to store the user input number
     // int number;
 
+    // Ask the user to enter a number
     // cout << "Enter a number to check positive or negative : ";
     // cin >> number;
 
+    // Check if the number is greater than or equal to 0
     // if (number >= 0)
     // {
     //     cout << "The number is positive";
     // }
     // else
     // {
+    //     // Executes when the number is less than 0
     //     cout << "The number is negative";
     // }
 
     // return 0;
 
-    // =========================================
-    // QUESTION 2 : Check Even or Odd Number
-    // =========================================
+    // =====================================================
+    // QUESTION 2 : Check Whether a Number is Even or Odd
+    // =====================================================
 
+    // Variable to store the number entered by the user
     // int number;
 
+    // Take input from the user
     // cout << "Enter a number to check odd or even : ";
     // cin >> number;
 
+    // Check if the remainder after division by 2 is 0
     // if (number % 2 == 0)
     // {
     //     cout << "The number is even";
     // }
     // else
     // {
+    //     // Executes if the number is not divisible by 2
     //     cout << "The number is odd";
     // }
 
     // return 0;
 
-    // =========================================
-    // QUESTION 3 : Check If Age is Eligible (18+)
-    // =========================================
+    // =====================================================
+    // QUESTION 3 : Check Eligibility Based on Age
+    // =====================================================
 
+    // Variable to store the user's age
     // int age;
 
+    // Ask the user to enter their age
     // cout << "Enter your age : ";
     // cin >> age;
 
+    // Check if age is 18 or above
     // if (age >= 18)
     // {
     //     cout << "You are eligible";
     // }
     // else
     // {
+    //     // Executes if age is below 18
     //     cout << "You are not eligible";
     // }
 
     // return 0;
 
-    // =========================================
-    // QUESTION 4 : Check If Number is Greater Than 10
-    // =========================================
+    // =====================================================
+    // QUESTION 4 : Check Whether a Number is Greater Than 10
+    // =====================================================
 
-    int number;
+    // Variable to store the user input
+    // int number;
 
-    cout << "Enter a number : ";
-    cin >> number;
+    // Take number input from the user
+    // cout << "Enter a number : ";
+    // cin >> number;
 
-    if (number > 10)
-    {
-        cout << "The number is greater than 10";
-    }
-    else
-    {
-        cout << "The number is 10 or smaller";
-    }
+    // Check if the number is greater than 10
+    // if (number > 10)
+    // {
+    //     cout << "The number is greater than 10";
+    // }
+    // else
+    // {
+    //     // Executes if the number is 10 or less
+    //     cout << "The number is 10 or smaller";
+    // }
 
-    return 0;
+    // return 0;
+
+    // =====================================================
+    // QUESTION 5 : Check Whether a Character is a Vowel or Consonant
+    // =====================================================
+
+    // Variable to store the alphabet entered by the user
+    // char alphabet;
+
+    // Ask the user to enter a character
+    // cout << "Enter the alphabet to check is it vowel or consonant : ";
+    // cin >> alphabet;
+
+    // Check if the character is a lowercase vowel
+    // if (alphabet == 'a' || alphabet == 'e' || alphabet == 'i' ||
+    //     alphabet == 'o' || alphabet == 'u')
+    // {
+    //     cout << "It's a vowel";
+    // }
+    // else
+    // {
+    //     // Executes if the character is not a vowel
+    //     cout << "It's a consonant";
+    // }
+
+    // return 0;
 }
